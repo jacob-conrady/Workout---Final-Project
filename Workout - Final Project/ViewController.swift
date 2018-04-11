28c922245e7848c20caa24e70e1fcbd2b81d8e12
Unknown <jacob.conrady.629@k12.friscoisd.org>
2018-04-11 11:34:03 -0500

    All story boards finished

--- Workout - Final Project/ViewController.swift
+++ Workout - Final Project/ViewController.swift
@@ -14,12 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var SignInBtn: UIButton!
+    @IBOutlet weak var SignUpBtn: UIButton!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func SignInSegue(_ sender: Any) {
+        performSegue(withIdentifier: "mainToSignIn", sender: SignInBtn)
+    }
+    @IBAction func SignUpSegue(_ sender: Any) {
+        performSegue(withIdentifier: "mainToSignUp", sender: SignUpBtn)
+    }
+    
 
 }
 
---
+++ Workout - Final Project/loginController.swift
@@ -0,0 +1,14 @@
+//
+//  loginController.swift
+//  Workout - Final Project
+//
+//  Created by Conrady, Jacob C on 4/11/18.
+//  Copyright © 2018 Conrady, Jacob C. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class loginController: UIViewController {
+    
+}
